09498a25b894d03129b21c467ceb64e87e5a20a4
ICS-Kawakatsu <kentaro.kawakatsu@ics-web.jp>
2025-04-08 20:32:24 +0900

    コメントを更新

--- sample/1_getImageData/script.js
+++ sample/1_getImageData/script.js
@@ -16,18 +16,21 @@ document.addEventListener("DOMContentLoaded", async () => {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   // 画素のデータを参照できるビュー配列、Uint8ClampedArrayを取得
   const data = imageData.data;
-  // 1ピクセルにつき4バイト（4要素）のUint8ClampedArrayデータが返却される。ピクセル数は単純にcanvas.width * canvas.heightでも取得可能
+
+  // 1ピクセルにつき4バイト（4要素）のUint8ClampedArrayデータが返却される。
   const pixelLength = data.length / 4;
   // すべてのピクセルについて処理
   for (let i = 0; i < pixelLength; i += 1) {
     // 注目ピクセルの最初の要素（R）のTypedArray内の位置
-    const pixelDataIndex = i * 4;
+    const pixelId = i * 4;
     // モノクロ化には様々な方法があるが、今回は単純に平均をとる
-    const average = (data[pixelDataIndex] + data[pixelDataIndex + 1] + data[pixelDataIndex + 2]) / 3;
-    data[pixelDataIndex] = average; // R
-    data[pixelDataIndex + 1] = average; // G
-    data[pixelDataIndex + 2] = average; // B
+    const average = (data[pixelId] + data[pixelId + 1] + data[pixelId + 2]) / 3;
+    data[pixelId] = average; // R
+    data[pixelId + 1] = average; // G
+    data[pixelId + 2] = average; // B
     // data[i + 3]はアルファのデータのためそのまま変更しない
   }
+
+  // 変更したピクセルのデータをCanvasにセット
   ctx.putImageData(imageData, 0, 0);
 })
